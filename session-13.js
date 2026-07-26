f517b150c982975ed8ba9bc6f9d61f10251baff9
Abhilash S <abhilashsubhash72@gmail.com>
2026-07-26 21:07:57 +0530

    start js

---
+++ session-13.js
@@ -0,0 +1,12 @@
+// type conversion ( explicit )
+
+// let age = Number('26')
+// console.log(age, typeof age);
+
+// console.log(Number('abc'));
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number(null));
+console.log(Number(''));
+
+
---
+++ session12.js
@@ -0,0 +1,28 @@
+console.log("hello");
+
+// Data types
+
+let a = 1
+let b = 'hello 1'
+let c = true
+let d = {
+    name: "abhilash",
+    class: "es6",
+    mob: 1235467,
+    isStudent: true
+}
+let e = [1, 'hello', true, { name: 'abhilash'}, ['orange, apple']]
+console.log(e, typeof e);
+
+// undefined
+let name;
+console.log(name);
+
+// null
+let name1 = null;
+console.log(typeof name1);
+
+const ab = 1;
+console.log(ab);
+
+
