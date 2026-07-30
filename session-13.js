7ca0e6b9e776385a193981e670078e2d7b3d037d
Abhilash S <abhilashsubhash72@gmail.com>
2026-07-30 22:59:59 +0530

    type conversion and coercion

--- session-13.js
+++ session-13.js
@@ -3,10 +3,49 @@
 // let age = Number('26')
 // console.log(age, typeof age);
 
-// console.log(Number('abc'));
-// console.log(Number(true));
-// console.log(Number(false));
-// console.log(Number(null));
-console.log(Number(''));
+// console.log(Number('abc'));     // NaN
+// console.log(Number(true));      // 1
+// console.log(Number(false));     // 0
+// console.log(Number(null));      // 0
+// console.log(Number(''));        // 0
+// console.log(Number());          // 0
+
+// console.log(String(true));         // true
+// console.log(String(false));        // false
+// console.log(String(22));           // 22
+// console.log(String(null));         // null
+// console.log(String(undefined));    // undefined
+
+// console.log(Boolean(true));         // true
+// console.log(Boolean(false));        // false
+// console.log(Boolean("hglkjh"));     // true
+// console.log(Boolean(null));         // false  
+// console.log(Boolean(NaN));          // false
+// console.log(Boolean(""));           // false
+
+// type coercion ( implicit )
+
+console.log("10" + 2);
+console.log("10" + 5 + 2);
+console.log("10" - 5);
+console.log("10" * 5);
+console.log(15 - "10");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
