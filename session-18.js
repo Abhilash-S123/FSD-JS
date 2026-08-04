3eed4e4d76760dae5a18f2d5db294dab688be2bc
Abhilash S <abhilashsubhash72@gmail.com>
2026-08-04 22:04:21 +0530

    callback function

--- session-18.js
+++ session-18.js
@@ -1,3 +1,15 @@
+function calculateDeliveryCharge (distance) {
+      return distance > 5 ? 50 : 20
+}
+
+const applyDiscount = (amount, discount = 0) => amount - amount * discount
+
+function placeYourOrder (order, discountType, callbackFn) {
+      let { customerName, distance, items } = order
+      finalAmount = discountType(items.reduce((sum, { price, quantity = 1 }) => sum + price * quantity, 0)) + calculateDeliveryCharge(distance)
+      callbackFn(customerName, finalAmount)
+}
+
 const order = {
     customerName: "Rahul",
     distance: 8,
@@ -6,3 +18,9 @@ const order = {
         { name: "Burger", price: 150 }
     ]
 }
+
+const festivalDiscount = (total) => applyDiscount(total, 0.10)
+
+placeYourOrder(order, festivalDiscount, (name, finalAmount) => { 
+    console.log(`Order confirmed for ${name} Pay Rs.${finalAmount}`);    
+})
