2f7a7554ec04ec2291feac9312f4f8f3a5f8cce0
Abhilash S <abhilashsubhash72@gmail.com>
2026-08-15 21:59:21 +0530

    dom continue

--- dom.js
+++ dom.js
@@ -1,5 +1,11 @@
 const card = document.querySelector('#card')
 const cardTitle = document.querySelector('#card-title')
+const form = document.querySelector('#form')
+const toggle = document.querySelector('#toggle')
+const userName = document.querySelector('#userName')
+const charCount = document.querySelector('#charCount')
+const submitBtn = document.querySelector('#submitBtn')
+
 
 console.log(cardTitle);
 console.log(cardTitle.textContent);
@@ -8,5 +14,21 @@ cardTitle.innerHTML = 'abhilash <H1>ffff</H1>'
 
 card.addEventListener('click', (e) => {
      card.classList.toggle('selected')
+     // card.classList.remove('selected')
+     // card.classList.add('selected')
 })
 
+toggle.addEventListener('click', (e) => {
+     e.stopPropagation()
+     form.classList.toggle('hidden')
+})
+
+userName.addEventListener('input', (e) => {
+     let userName =  e.target.value 
+      charCount.textContent = `${userName.length} / 10 characters`  
+      charCount.style.color = userName.length > 10 ? 'red' : 'green'
+})
+
+submitBtn.addEventListener('submit', (e) => {
+
+})
